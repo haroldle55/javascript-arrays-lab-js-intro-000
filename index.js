9c6b0fb8986cd72fbc377be62fc2fee9664446c3
Harold Lee <harry.yg.lee@gmail.com>
2020-06-28 15:48:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
-function destructivelyAppendKitten(name) {
+function destructivelyAppendKitten(name){
   kittens.push(name)
 }
 function destructivelyPrependKitten(name){
   kittens.unshift(name)
 }
-function destructivelyRemoveLastKitten(name){
+function destructivelyRemoveLastKitten(){
   kittens.pop(name)
 }
-function destructivelyRemoveFirstKitten(name){
+function destructivelyRemoveFirstKitten(){
   kittens.shift(name)
 }
 function appendKitten(name){
-  return[...kittens,name]
+  return [...kittens,name]
 }
 function prependKitten(name){
   return [name,...kittens]
 }
 function removeLastKitten(){
-return kittens.slice(0,kittens.length - 1)
-
+  return kittens.slice(0,kittens.length -1)
 }
 function removeFirstKitten(){
-return kittens.slice(1)
+  return kittens.slice(1)
 }
